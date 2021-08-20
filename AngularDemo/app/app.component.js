c130aa2ccb7e578a5a2c26d5b10d12bb1637881a
Touhid <nasimuzzamant@gmail.com>
2021-08-20 23:55:43 +0600

    interpolation component to view

--- AngularDemo/app/app.component.js
+++ AngularDemo/app/app.component.js
@@ -10,13 +10,17 @@ var core_1 = require("@angular/core");
 var AppComponent = /** @class */ (function () {
     function AppComponent() {
         this.pageHeader = "Student Details";
-        //appHeading: string = "This is my first angular2 app"; 
+        this.appHeading = "This is my first angular2 app";
+        this.firstName = "Faisal";
+        this.lastName = "Nuri";
+        this.imagePath = "https://bangladesh.gov.bd/themes/responsive_npf/templates/bangladesh/images/logo_bn.png";
+        this.accident = null;
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
             //templateUrl: './app/app.component.html' 
-            template: "<div>\n        <h1>{{ pageHeader }}</h1>\n        <studentDetails></studentDetails>\n        </div> "
+            template: "<div>\n        <h1>{{ pageHeader }}</h1>\n        <h1>{{ 'Application Heading : ' + appHeading}}</h1>\n        <p> Now I am in {{0*2+2*2*2-0/2}} lecture.</p>\n        <p> My mother name is {{firstName + ' ' + lastName}}.</p>\n        <p> Bangladesh <img src=\"{{imagePath}}\"/> </p>\n        <p> {{accident ? accident : 'This is ternary operator'}}.</p>\n        <studentDetails></studentDetails>\n        </div> "
         })
     ], AppComponent);
     return AppComponent;
--- AngularDemo/app/app.component.ts
+++ AngularDemo/app/app.component.ts
@@ -4,13 +4,21 @@
     //templateUrl: './app/app.component.html' 
     template: `<div>
         <h1>{{ pageHeader }}</h1>
+        //<h1>{{ 'Application Heading : ' + appHeading}}</h1>
+        //<p> Now I am in {{0*2+2*2*2-0/2}} lecture.</p>
+        //<p> My mother name is {{firstName + ' ' + lastName}}.</p>
+        //<p> Bangladesh <img src="{{imagePath}}"/> </p>
+        //<p> {{accident ? accident : 'This is ternary operator'}}.</p>
         <studentDetails></studentDetails>
         </div> `
 })
 export class AppComponent {
  
     pageHeader: string = "Student Details"; 
-    //appHeading: string = "This is my first angular2 app"; 
-    
+    //appHeading: string = "This is my first angular2 app";
+    //firstName: string = "Faisal";
+    //lastName: string = "Nuri";
+    //imagePath: string = "https://bangladesh.gov.bd/themes/responsive_npf/templates/bangladesh/images/logo_bn.png";
+    //accident: string = null;
 }  
 
