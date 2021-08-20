1ab4a6b4c09c50c009030995db30eedb41712ac3
Touhid <nasimuzzamant@gmail.com>
2021-08-20 16:37:15 +0600

    Add project files.

---
+++ AngularDemo/app/app.component.ts
@@ -0,0 +1,13 @@
+﻿import { Component } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    templateUrl: './app/app.component.html' 
+    //template: '<div>    Title: {{ appTitle }}<br />    </div>'
+})
+export class AppComponent {
+ 
+    appTitle: string = "Hello World!"; 
+    appHeading: string = "This is my first angular2 app"; 
+    
+}  
+
---
+++ AngularDemo/readme.txt
@@ -0,0 +1,7 @@
+﻿This is a basic angular2 web app template example with minimum dependencies installed
+
+Installation Instruction:
+
+-right click on package.json file and click on "Restore packages" and wait for the installation files to be downloaded 
+
+-now build the solution. (If you get any error on build try to build it again)
